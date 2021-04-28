20d8fe62c010419b4721e0d37df6685a957ddbdf
Eugene <jenyagvozd@mail.ru>
2021-04-28 19:31:08 +0300

    Include BouncyCastle libraries, try to implement RSA encryption

--- EncryptionApp/Model/AsymmetricEncryption.cs
+++ EncryptionApp/Model/AsymmetricEncryption.cs
@@ -22,26 +22,31 @@ namespace EncryptionApp.Model
         RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
         RSAParameters publicKeyInfo = new RSAParameters();
         RSAParameters privateKeyInfo = new RSAParameters();
-        public void TestMethod() {
-            RsaKeyPairGenerator rsa = new RsaKeyPairGenerator();
-            rsa.Init(new KeyGenerationParameters(new Org.BouncyCastle.Security.SecureRandom(), 1024));
-            AsymmetricCipherKeyPair pair = rsa.GenerateKeyPair();
-            
-        }
         public AsymmetricEncryption() {
             this.publicKeyInfo = rsa.ExportParameters(false);
             this.privateKeyInfo = rsa.ExportParameters(true);
         }
         public override byte[] Encrypt(byte[] data, string _, string privateKeyInfo) {
-            TestMethod();
+            string privateKey = $"-----BEGIN RSA PRIVATE KEY-----{privateKeyInfo}-----END RSA PRIVATE KEY-----";
+            rsa = RSAKeys.ImportPrivateKey(privateKey);
             return data;
         }
         public override byte[] Decrypt(byte[] data, string _, string privateKey) {
             throw new NotImplementedException();
         }
         public override string[] CreateKeys() {
-            MessageBox.Show("Библиотеки C# не располагают классами, предоставляющими средства вывода RSA-ключей по сертификату X509. \nНиже выведены байтовые представления экземпляров класса RSAParameters — вычисляющего ключи");
-            return null;
+            RsaKeyPairGenerator rsa = new RsaKeyPairGenerator();
+            rsa.Init(new KeyGenerationParameters(new Org.BouncyCastle.Security.SecureRandom(), 1024));
+            AsymmetricCipherKeyPair pair = rsa.GenerateKeyPair();
+            PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(pair.Private);
+            
+            SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(pair.Public);
+            byte[] privateBytes = privateKeyInfo.PrivateKeyData.ToAsn1Object().GetDerEncoded();
+            byte[] publicBytes = publicKeyInfo.ToAsn1Object().GetDerEncoded();
+            return new string[] {
+                Convert.ToBase64String(publicBytes),
+                Convert.ToBase64String(privateBytes)
+            };
         } 
     }
 }
