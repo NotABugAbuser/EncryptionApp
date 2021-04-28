fda10106c72c696ec52bf9c2a2cbc0dffec0a010
Eugene <jenyagvozd@mail.ru>
2021-04-28 12:10:48 +0300

    Set base MVVM structures

---
+++ EncryptionApp/ViewModel/MainWindowVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionApp.ViewModel
+{
+    class MainWindowVM : INotifyPropertyChanged {
+        public void OnPropertyChanged([CallerMemberName] string property = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
