e2b431aad46208c55331efdeca6aaa6f4b30f082
Eugene <jenyagvozd@mail.ru>
2021-04-28 16:30:10 +0300

    Start implementing symmetric encryption

--- EncryptionApp/Model/SymmetricEncryption.cs
+++ EncryptionApp/Model/SymmetricEncryption.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,17 @@ namespace EncryptionApp.Model
     class SymmetricEncryption : Encryption
     {
         public override byte[] Encrypt(byte[] data, string key, string iv) {
-            throw new NotImplementedException();
+            byte[] keyBlob = Convert.FromBase64String(key);
+            byte[] ivBlob = Convert.FromBase64String(iv);
+            using (Aes aes = Aes.Create()) {
+                aes.KeySize = 128;
+                aes.Key = keyBlob;
+                aes.BlockSize = 128;
+                aes.IV = ivBlob;
+                using (ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV)) {
+                    
+                }
+            }
         }
         public override byte[] Decrypt(byte[] data, string key, string iv) {
             throw new NotImplementedException();
